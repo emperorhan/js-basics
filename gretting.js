50cf37f6496d1291f0716e873144889df0a68091
emperorhan <iny27@naver.com>
2019-01-10 22:03:31 +0900

    Last commit

--- gretting.js
+++ gretting.js
@@ -5,6 +5,22 @@ const form = document.querySelector(".js-form"),
 const USER_LS = "currentUser",
   SHOWING_CN = "showing";
 
+function savaName(text){
+    localStorage.setItem(USER_LS, text);
+}
+
+function handleSubmit(event){
+    event.preventDefault();
+    const currentValue = input.value;
+    paintGreeting(currentValue);
+    savaName(currentValue);
+}
+
+function askForName(){
+    form.classList.add(SHOWING_CN);
+    form.addEventListener("submit", handleSubmit);
+}
+
 function paintGreeting(text) {
   form.classList.remove(SHOWING_CN);
   greeting.classList.add(SHOWING_CN);
@@ -21,7 +37,7 @@ function loadName() {
 }
 
 function init() {
-    loadName();
+  loadName();
 }
-
+askForName();
 init();
